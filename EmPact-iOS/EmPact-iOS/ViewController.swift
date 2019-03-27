6c3f2e04de3cb85fb9524835371e4e5d3eaacd3e
audreywelch <welch.audreyl@gmail.com>
2019-03-27 17:58:02 -0500

    Project skeleton created

---
+++ EmPact-iOS/EmPact-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmPact-iOS
+//
+//  Created by Audrey Welch on 3/27/19.
+//  Copyright © 2019 EmPact. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
