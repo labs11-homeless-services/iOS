6dff763300bf311a92584f1ed805f8401e322faf
audreywelch <welch.audreyl@gmail.com>
2019-03-29 15:42:02 -0500

    Built out Collection View views

---
+++ EmPact-iOS/EmPact-iOS/View Controllers/CategoriesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CategoriesViewController.swift
+//  EmPact-iOS
+//
+//  Created by Audrey Welch on 3/29/19.
+//  Copyright © 2019 EmPact. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Make Collection View Outlet
+        // Set Delegate
+        // Set DataSource
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EmPact-iOS/EmPact-iOS/View Controllers/ServiceDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ServiceDetailViewController.swift
+//  EmPact-iOS
+//
+//  Created by Audrey Welch on 3/29/19.
+//  Copyright © 2019 EmPact. All rights reserved.
+//
+
+import UIKit
+
+class ServiceDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
