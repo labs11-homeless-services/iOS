73f7671294ae57b268b3808371d577a6907c4d7e
audreywelch <welch.audreyl@gmail.com>
2019-03-28 19:15:52 -0500

    New project with gitignore file added

---
+++ EmPact-iOS/EmPact-iOS/Helpers/AppearanceHelper.swift
@@ -0,0 +1,13 @@
+//
+//  AppearanceHelper.swift
+//  EmPact-iOS
+//
+//  Created by Audrey Welch on 3/28/19.
+//  Copyright © 2019 EmPact. All rights reserved.
+//
+
+import Foundation
+
+enum Appearance {
+    
+}
