e259f87dff837e733dd4440a57510a4241e7ad12
JDB0rg <filthyfeather@gmail.com>
2019-03-29 12:29:10 -0600

    Added Categories View Controller and Collection View Cell

---
+++ EmPact-iOS/EmPact-iOS/View Controllers/CategoriesViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CategoriesViewController.swift
+//  EmPact-iOS
+//
+//  Created by Madison Waters on 3/29/19.
+//  Copyright © 2019 EmPact. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController, UICollectionViewDelegate, UICollectaionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Make Collection View Outlet
+        // Set Delegate
+        // Set DataSource
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
