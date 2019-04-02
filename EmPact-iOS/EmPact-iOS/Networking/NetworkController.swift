8eb2f06893157a2ed3d1cb9304e9599fc0885723
JDB0rg <filthyfeather@gmail.com>
2019-04-02 17:01:08 -0600

    Populate icons with correct icon images

--- EmPact-iOS/EmPact-iOS/Networking/NetworkController.swift
+++ EmPact-iOS/EmPact-iOS/Networking/NetworkController.swift
@@ -46,9 +46,11 @@ class NetworkController {
                 let decodedResponse = try jsonDecoder.decode(Categories.self, from: data)
                 print("Network decodedResponse: \(decodedResponse)")
                 
-                //self.categoryNames = decodedResponse
                 let categories = decodedResponse.categoryName
-                self.categoryNames = categories
+                let capitalizedCategories = categories.map {$0.capitalized}
+                
+                self.categoryNames = capitalizedCategories
+                print("Network Categories: \(categories)")
                 completion(nil)
             } catch {
                 completion(error)
