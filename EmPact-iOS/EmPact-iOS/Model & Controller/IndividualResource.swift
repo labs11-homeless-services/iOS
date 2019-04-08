59025c13e56ffa135fede7542c2a5525ca905fba
JDB0rg <filthyfeather@gmail.com>
2019-04-08 14:51:34 -0600

    Committing to pull from master.

--- EmPact-iOS/EmPact-iOS/Model & Controller/IndividualResource.swift
+++ EmPact-iOS/EmPact-iOS/Model & Controller/IndividualResource.swift
@@ -2,7 +2,7 @@
 //  IndividualResource.swift
 //  EmPact-iOS
 //
-//  Created by Audrey Welch on 4/3/19.
+//  Created by Madison Waters on 4/8/19.
 //  Copyright © 2019 EmPact. All rights reserved.
 //
 
@@ -20,32 +20,32 @@ struct IndividualResource: Decodable {
     var phone: Int?
     var postalCode: String?
     
-//    private enum CodingKeys: String, CodingKey {
-//        case address
-//        case city
-//        case keywords
-//        case name
-//        case state
-//        case latitude
-//        case longitude
-//        case phone
-//        case postalCode = "postal code"
-//    }
-//
-//    init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//        address = try container.decode(String.self, forKey: .address)
-//        city = try container.decode(String.self, forKey: .city)
-//        keywords = try container.decode(String.self, forKey: .keywords)
-//        name = try container.decode(String.self, forKey: .name)
-//        state = try container.decode(String.self, forKey: .state)
-//        latitude = try container.decode(String.self, forKey: .latitude)
-//        longitude = try container.decode(String.self, forKey: .longitude)
-//
-//        if let phoneValue = try? container.decode(Int.self, forKey: .phone) {
-//            phone = String(phoneValue)
-//        } else {
-//            phone = try container.decode(String.self, forKey: .phone)
-//        }
-//    }
+    //    private enum CodingKeys: String, CodingKey {
+    //        case address
+    //        case city
+    //        case keywords
+    //        case name
+    //        case state
+    //        case latitude
+    //        case longitude
+    //        case phone
+    //        case postalCode = "postal code"
+    //    }
+    //
+    //    init(from decoder: Decoder) throws {
+    //        let container = try decoder.container(keyedBy: CodingKeys.self)
+    //        address = try container.decode(String.self, forKey: .address)
+    //        city = try container.decode(String.self, forKey: .city)
+    //        keywords = try container.decode(String.self, forKey: .keywords)
+    //        name = try container.decode(String.self, forKey: .name)
+    //        state = try container.decode(String.self, forKey: .state)
+    //        latitude = try container.decode(String.self, forKey: .latitude)
+    //        longitude = try container.decode(String.self, forKey: .longitude)
+    //
+    //        if let phoneValue = try? container.decode(Int.self, forKey: .phone) {
+    //            phone = String(phoneValue)
+    //        } else {
+    //            phone = try container.decode(String.self, forKey: .phone)
+    //        }
+    //    }
 }
