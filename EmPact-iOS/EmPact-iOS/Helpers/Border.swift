4adb6043b32d6d9620373b56c9a5c0f0982eff87
JDB0rg <filthyfeather@gmail.com>
2019-04-17 09:47:12 -0600

    Add UI Styling for subcategories and search resutls views

---
+++ EmPact-iOS/EmPact-iOS/Helpers/Border.swift
@@ -0,0 +1,39 @@
+//
+//  Border.swift
+//  EmPact-iOS
+//
+//  Created by Madison Waters on 4/16/19.
+//  Copyright © 2019 EmPact. All rights reserved.
+//
+
+import UIKit
+
+extension CALayer {
+    
+    func addBorder(edge: UIRectEdge, color: UIColor, thickness: CGFloat) {
+        
+        let border = CALayer();
+        
+        switch edge {
+        case UIRectEdge.top:
+            border.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: thickness)
+            break
+        case UIRectEdge.bottom:
+            border.frame = CGRect(x:0, y:self.frame.height - thickness, width:self.frame.width, height:thickness)
+            break
+        case UIRectEdge.left:
+            border.frame = CGRect(x:0, y:0, width: thickness, height: self.frame.height)
+            break
+        case UIRectEdge.right:
+            border.frame = CGRect(x:self.frame.width - thickness, y: 0, width: thickness, height:self.frame.height)
+            break
+        default:
+            break
+        }
+        
+        border.backgroundColor = color.cgColor;
+        
+        self.addSublayer(border)
+    }
+    
+}
